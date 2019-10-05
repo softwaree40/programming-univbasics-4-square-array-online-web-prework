50cef7bb9cdda7cab03d0e17e51282cf1b13996c
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-05 23:01:08 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+    new_array = []
+    i = 0
+    while i < array.length
+      char = array[i]
+      new_array << char**2
+    i+=1
+  end
+    return new_array
 end
